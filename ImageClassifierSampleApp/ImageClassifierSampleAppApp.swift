3afd5735cbbfe7933732401bf70014778f5d853b
pakuVision <boardguy.vision@gmail.com>
2025-05-25 19:28:25 +0900

    Initial Commit

---
+++ ImageClassifierSampleApp/ImageClassifierSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassifierSampleAppApp.swift
+//  ImageClassifierSampleApp
+//
+//  Created by boardguy.vision on 2025/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassifierSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
